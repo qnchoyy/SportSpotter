17685f1bfc8a6691c6840a9b6c352502fa2c5684
qnchoyy <qni191488@abv.bg>
2025-12-04 15:19:32 +0200

    feat: add protected /users/me endpoint

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,11 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMyProfile(@GetUser() user: User): User {
+    return user;
+  }
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
