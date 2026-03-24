93720561320e4010b4c17770a840dcd4bfcf2cdf
qnchoyy <qni191488@abv.bg>
2026-03-24 13:52:24 +0200

    feat: implement match details page with join functionality and loading state

--- client/src/pages/MatchDetailsPage.tsx
+++ client/src/pages/MatchDetailsPage.tsx
@@ -1,10 +1,37 @@
 import { useParams } from "react-router-dom";
 import { useMatch } from "../hooks/useMatch";
 import Spinner from "../components/ui/Spinner";
+import { matchesService } from "../services/matches";
+import { useEffect, useState } from "react";
+import type { Participant } from "../types/participant";
+import { useAuth } from "../contexts/AuthContext";
+import FootballField from "../components/fields/FootballField";
+import TennisCourt from "../components/fields/TennisCourt";
+import MatchHeader from "../components/matches/MatchHeader";
+import MatchInfoPanel from "../components/matches/MatchInfoPanel";
 
 const MatchDetailsPage = () => {
+  const [isJoining, setIsJoining] = useState(false);
+  const [participants, setParticipants] = useState<Participant[]>([]);
+
   const { id } = useParams();
   const { match, loading, error } = useMatch(id!);
+  const { user } = useAuth();
+
+  const fetchParticipants = async () => {
+    try {
+      if (!id) return;
+
+      const data = await matchesService.getParticipants(id);
+      setParticipants(data);
+    } catch (error) {
+      console.error("Failed to fetch participants", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchParticipants();
+  }, [id]);
 
   if (loading) {
     return (
@@ -22,37 +49,67 @@ const MatchDetailsPage = () => {
     );
   }
 
-  const formattedDate = new Date(match.startTime).toLocaleString("en-GB", {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
+  const isJoined = participants.some((p) => p.user.id === user?.id);
   const maxPlayers = match.playersPerTeam * match.numberOfTeams;
+  const isFull = participants.length >= maxPlayers;
+  const isNotLoggedIn = !user;
+
+  const handleJoin = async (team: number) => {
+    if (isNotLoggedIn || isJoined || isFull) return;
+
+    try {
+      setIsJoining(true);
+      await matchesService.joinMatch(id!, team);
+      await fetchParticipants();
+    } catch (error) {
+      console.error("failed to join", error);
+    } finally {
+      setIsJoining(false);
+    }
+  };
 
   return (
-    <div className="mx-auto max-w-3xl px-4 py-16">
-      <div className="rounded-2xl border border-slate-700 bg-slate-900/50 p-8">
-        <h1 className="text-3xl font-bold capitalize">{match.sport} Match</h1>
+    <div className="mx-auto max-w-5xl px-4 py-1">
+      <div className="space-y-8">
+        <MatchHeader match={match} />
 
-        <div className="mt-4 text-gray-400">
-          {match.venue.name} • {match.venue.city}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
+          <div className="md:col-span-2 h-full">
+            {match.sport === "football" && (
+              <FootballField
+                playersPerTeam={match.playersPerTeam}
+                participants={participants}
+                onJoin={handleJoin}
+                currentUserId={user?.id}
+                isJoining={isJoining}
+              />
+            )}
+
+            {match.sport === "tennis" && (
+              <TennisCourt
+                playersPerTeam={match.playersPerTeam}
+                participants={participants}
+                onJoin={handleJoin}
+                currentUserId={user?.id}
+                isJoining={isJoining}
+              />
+            )}
+          </div>
+
+          <MatchInfoPanel
+            totalPlayers={participants.length}
+            maxPlayers={maxPlayers}
+            isJoined={isJoined}
+            isFull={isFull}
+            isNotLoggedIn={isNotLoggedIn}
+            minSkillLevel={match.minSkillLevel}
+            maxSkillLevel={match.maxSkillLevel}
+            organizerUsername={match.organizer.username}
+            status={match.status}
+            venueAddress={match.venue.address}
+            venueCity={match.venue.city}
+          />
         </div>
-
-        <div className="mt-2 text-gray-400">{formattedDate}</div>
-
-        <div className="mt-6 text-gray-300">
-          {match.playersPerTeam}v{match.playersPerTeam} • {match.numberOfTeams}{" "}
-          teams
-        </div>
-
-        <div className="text-gray-300">
-          Skill: {match.minSkillLevel} - {match.maxSkillLevel}
-        </div>
-
-        <div className="mt-4 text-gray-300">Max players: {maxPlayers}</div>
       </div>
     </div>
   );
